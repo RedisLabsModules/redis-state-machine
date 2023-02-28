cfda9255d91be274948ff59f6c1d9eab0a2318cc
Guy Korland <gkorland@gmail.com>
2023-02-28 13:14:53 +0200

    initial cleanup (#2)

--- src/function_get.rs
+++ src/function_get.rs
@@ -1,29 +1,23 @@
 use crate::types::StateMachine;
 use crate::REDIS_SM_TYPE;
-use redis_module::{key::RedisKey, Context, RedisError, RedisResult, RedisString, RedisValue};
+use redis_module::{
+    key::RedisKey, Context, NextArg, RedisError, RedisResult, RedisString, RedisValue,
+};
 
 pub(crate) fn get(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    let args = args.into_boxed_slice();
     if args.len() < 2 {
         return Err(RedisError::WrongArity);
     }
-    let key = &args[1];
+    let mut args = args.into_iter().skip(1);
+    let key = args.next_arg()?;
 
     let rkey = RedisKey::open(ctx.ctx, &key);
-    let val = rkey.get_value::<StateMachine>(&REDIS_SM_TYPE);
+    let v = rkey.get_value::<StateMachine>(&REDIS_SM_TYPE)?;
 
-    match val {
-        Err(e) => {
-            return Err(e);
-        }
-        Ok(v) => {
-            if v.is_none() {
-                return Ok(RedisValue::Null);
-            } else {
-                let sm = val.unwrap();
-                let rval = serde_json::to_string(&sm)?;
-                return Ok(RedisValue::BulkString(rval));
-            }
-        }
+    if v.is_none() {
+        Ok(RedisValue::Null)
+    } else {
+        let rval = serde_json::to_string(&v)?;
+        Ok(RedisValue::BulkString(rval))
     }
 }
--- src/function_state.rs
+++ src/function_state.rs
@@ -1,57 +1,43 @@
 use crate::types::StateMachine;
 use crate::REDIS_SM_TYPE;
-use redis_module::{key::RedisKey, Context, RedisError, RedisResult, RedisString, RedisValue};
+use redis_module::{
+    key::RedisKey, Context, NextArg, RedisError, RedisResult, RedisString, RedisValue,
+};
 
 pub(crate) fn current_state(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    let args = args.into_boxed_slice();
     if args.len() != 2 {
         return Err(RedisError::WrongArity);
     }
-    let key = &args[1];
+    let mut args = args.into_iter().skip(1);
+
+    let key = args.next_arg()?;
 
     let rkey = RedisKey::open(ctx.ctx, &key);
-    let val = rkey.get_value::<StateMachine>(&REDIS_SM_TYPE);
+    let value = rkey.get_value::<StateMachine>(&REDIS_SM_TYPE)?;
 
-    match val {
-        Err(e) => {
-            return Err(e);
-        }
-        Ok(v) => {
-            if v.is_none() {
-                return Ok(RedisValue::Null);
-            } else {
-                let sm = val.unwrap();
-                let vvv = sm.unwrap();
-                return Ok(RedisValue::SimpleString(vvv.current().to_string()));
-            }
-        }
-    }
+    value.map_or_else(
+        || Ok(RedisValue::Null),
+        |sm| Ok(RedisValue::SimpleString(sm.current().to_string())),
+    )
 }
 
 pub(crate) fn states(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    let args = args.into_boxed_slice();
     if args.len() != 2 {
         return Err(RedisError::WrongArity);
     }
-    let key = &args[1];
+    let mut args = args.into_iter().skip(1);
+    let key = args.next_arg()?;
 
     let rkey = RedisKey::open(ctx.ctx, &key);
-    let val = rkey.get_value::<StateMachine>(&REDIS_SM_TYPE);
-    match val {
-        Err(e) => {
-            return Err(e);
+    let value = rkey.get_value::<StateMachine>(&REDIS_SM_TYPE)?;
+
+    if let Some(sm) = value {
+        let mut keys: Vec<RedisValue> = Vec::new();
+        for x in sm.map().keys() {
+            keys.push(RedisValue::SimpleString(x.to_string()));
         }
-        Ok(v) => {
-            if v.is_none() {
-                return Ok(RedisValue::Null);
-            } else {
-                let sm = v.unwrap();
-                let mut keys: Vec<RedisValue> = Vec::new();
-                for x in sm.map().keys() {
-                    keys.push(RedisValue::SimpleString(x.to_string()));
-                }
-                return Ok(RedisValue::Array(keys));
-            }
-        }
+        Ok(RedisValue::Array(keys))
+    } else {
+        Ok(RedisValue::Null)
     }
 }
