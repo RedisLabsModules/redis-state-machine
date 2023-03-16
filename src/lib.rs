a3637510a7a1355bba4a4a33237583ee4b316917
Chayim I. Kirshen <c@kirshen.com>
2023-03-16 15:04:57 +0200

    cleaning up the API aroudn current states

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ pub const REDIS_SM_TYPE_VERSION: i32 = 1;
 pub const MODULE_NAME: &str = "RedisStateMachine";
 pub const MODULE_TYPE: &str = "RedisStateMachine";
 
-mod function_delete;
 mod function_get;
 mod function_set;
 mod function_state;
@@ -21,13 +20,11 @@ redis_module! {
     commands: [
         ["SM.GET", function_get::get, "readonly", 0, 0, 0],
         ["SM.SET", function_set::set, "write deny-oom", 1, 1, 1],
-        ["SM.CURRENT", function_state::current_state, "readonly", 0, 0, 0],
-        ["SM.STATES", function_state::states, "readonly", 0, 0, 0],
-        ["SM.DEL", function_delete::delete, "write", 1, 1, 1],
+        ["SM.STATE", function_state::state, "readonly", 0, 0, 0],
+        // ["SM.STATES", function_state::states, "readonly", 0, 0, 0],
         ["SM.CREATE", function_set::create, "write deny-oom", 1, 1, 1],
         ["SM.TEMPLATE", function_get::template, "readonly", 0, 0, 0],
-        // ["SM.FORCE", function_set::force_set, "write deny-oom", 1, 1, 1],
         ["SM.RESET", function_set::reset, "write deny-oom", 1, 1, 1],
-        ["SM.TRANSITION", function_state::transition, "write deny-oom", 1, 1, 1],
+        ["SM.MUTATE", function_state::mutate, "write deny-oom", 1, 1, 1],
     ],
 }
